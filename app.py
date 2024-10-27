51c87c6a6c29bfc262b5dc08ac767358d2e5ebda
versklin1nastya <izmaskakoralina@gmail.com>
2024-10-27 16:49:52 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("Startpage.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/enter")
+def enter():
+    return render_template("enter.html")
+
+
+@app.route("/Mainpage")
+def Mainpage():
+    return render_template("Mainpage.html")
+
+@app.route("/account")
+def account():
+    return render_template("account.html")
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
+
