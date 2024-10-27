68d1a9a92f2c231df9dc8eef24718ad7269910d9
versklin1nastya <izmaskakoralina@gmail.com>
2024-10-27 17:08:06 +0300

    Delete ansel directory

--- ansel/karta.js
+++
@@ -1,8 +0,0 @@
-ymaps.ready(init);
-
-function init() {
-    var myMap = new ymaps.Map("map", {
-        center: [55.753607, 37.620920],
-        zoom: 15
-    });
-}
--- ansel/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('login-button').addEventListener('click', function() {
-  document.getElementById('modal').style.display = 'block';
-});
-
-document.getElementsByClassName('close')[0].addEventListener('click', function() {
-  document.getElementById('modal').style.display = 'none';
-});
